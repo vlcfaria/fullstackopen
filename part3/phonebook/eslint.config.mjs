66c99f3f9ddfc32a55d0e016e0653fcdcb685a45
Vinicius Faria <artixsproductions@gmail.com>
2024-11-26 13:01:51 -0300

    uibuild

---
+++ part3/phonebook/eslint.config.mjs
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+      },
+      ecmaVersion: "latest",
+    },
+  },
+]
