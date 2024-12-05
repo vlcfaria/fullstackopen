5d4ddf117ec283b006dc206324ef50e73c6c88a2
Vinicius Faria <artixsproductions@gmail.com>
2024-12-05 18:09:18 -0300

    finished 4.7

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = blogs => {
     return 1
 }
@@ -12,8 +14,31 @@ const favoriteBlog = blogs => {
     return blogs.reduce((max, cur) => cur.likes > max.likes ? cur : max)
 }
 
+const mostBlogs = blogs => {
+    if (blogs.length == 0) return undefined
+
+    const counted = _.countBy(blogs, 'author')
+    maxAuthor =  _.max(_.entries(counted), _.last)
+    return { author: maxAuthor[0], blogs: maxAuthor[1] }
+}
+
+const mostLikes = blogs => {
+    if (blogs.length == 0) return undefined
+
+    result = _.chain(blogs) //Chain calls with value() at last
+              .groupBy('author') //Group by author
+              .mapValues(group => _.sumBy(group, 'likes')) //Sum all likes
+              .entries() // transform object into [key, value] pairs
+              .maxBy(_.last) // Get maximum from last element
+              .value() // return value
+    
+    return { author: result[0], likes: result[1] }
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
